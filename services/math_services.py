08b55b3b7f90f9c70a64ef70f4c1a7e9aa3b4f2c
Rodica Elena Rosca <Rodica-Elena.Rosca@endava.com>
2025-07-21 17:28:30 +0300

    Integrated redis with docker

--- services/math_services.py
+++ services/math_services.py
@@ -1,24 +1,54 @@
+import redis
+import os
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis") 
+r = redis.Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
+
 class MathService:
     @staticmethod
     def factorial(n: int) -> int:
+        cache_key = f"factorial:{n}"
+        cached = r.get(cache_key)
+
+        if cached:
+            return int(cached)
+        
         if n < 0:
             raise ValueError("Factorial is not defined for negative numbers.")
         result = 1
         
         for i in range(2, n + 1):
             result *= i
+            r.set(cache_key, result)
+
         return result
 
     @staticmethod
     def fibonacci(n: int) -> int:
+        cache_key = f"fib:{n}"
+        cached = r.get(cache_key)
+
+        if cached:
+            return int(cached)
+
         if n < 0:
             raise ValueError("Input must be a non-negative integer.")
         a, b = 0, 1
 
         for _ in range(n):
             a, b = b, a + b
+            r.set(cache_key, a)
         return a
     
     @staticmethod
     def power(base: float, exponent: float) -> float:
-        return base ** exponent
+        cache_key = f"power:{base}:{exponent}"
+        cached = r.get(cache_key)
+
+        if cached:
+            return float(cached)
+        
+        result = base ** exponent
+        r.set(cache_key, result)
+
+        return result
