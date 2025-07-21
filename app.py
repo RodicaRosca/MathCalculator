0ce722b87e29e42c3b4b409c7be86d42797386ad
Rodica Elena Rosca <Rodica-Elena.Rosca@endava.com>
2025-07-21 17:11:40 +0300

    Integrated Prometheus&Grafana

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 from db.database import Base, engine
 from controllers.controllers import router as math_router
 from controllers.frontend_controllers import router as frontend_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Math Microservice")
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 app.include_router(frontend_router)
 app.include_router(math_router)
--- kafka_logging.py
+++ kafka_logging.py
@@ -1,17 +1,33 @@
-from kafka import KafkaProducer
+# from kafka import KafkaProducer
+# import json
+# import os
+
+# KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
+
+# producer = KafkaProducer(
+#     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+#     value_serializer=lambda v: json.dumps(v).encode('utf-8')
+# )
+
+# def log_to_kafka(log_data):
+#     try:
+#         producer.send('apilogs', log_data)
+#         producer.flush()
+#     except Exception as e:
+#         print(f"Kafka logging failed: {e}")
+
 import json
+from kafka import KafkaProducer
 import os
-
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
-
-producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
 
 def log_to_kafka(log_data):
     try:
+        producer = KafkaProducer(
+            bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092"),
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
         producer.send('apilogs', log_data)
         producer.flush()
+        producer.close()
     except Exception as e:
         print(f"Kafka logging failed: {e}")
