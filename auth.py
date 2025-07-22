a26bd65bb02a602fd0b0f2745544f7d25eb227d0
Rodica Elena Rosca <Rodica-Elena.Rosca@endava.com>
2025-07-22 10:31:41 +0300

    Added create_user method

--- auth.py
+++ auth.py
@@ -1,8 +1,35 @@
+# import os
+# from fastapi import Depends, HTTPException, status
+# from fastapi.security import OAuth2PasswordBearer
+# from jose import JWTError, jwt
+
+
+# SECRET_KEY = os.getenv("SECRET_KEY")
+
+# if not SECRET_KEY:
+#     raise RuntimeError("SECRET_KEY environment variable must be set! See the project README.")
+
+# ALGORITHM = "HS256"
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# def verify_token(token: str = Depends(oauth2_scheme)):
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         return payload 
+#     except JWTError:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Could not validate credentials"
+#         )
+
 import os
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-
+from sqlalchemy.orm import Session
+from db.database import SessionLocal
+from models.request_log import User
+from passlib.context import CryptContext
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
@@ -11,11 +38,26 @@ if not SECRET_KEY:
 
 ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def authenticate_user(db, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    
+    if user and pwd_context.verify(password, user.hashed_password):
+        return user
+    return None
 
 def verify_token(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload 
+        return payload
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
---
+++ register_user.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from models.request_log import User
+from db.database import SessionLocal
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_user(username: str, password: str, role="user"):
+    db: Session = SessionLocal()
+    hashed = pwd_context.hash(password)
+    user = User(username=username, hashed_password=hashed, role=role)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    db.close()
+    print(f"Created user: {username}")
+
+if __name__ == "__main__":
+    create_user("admin", "changeme123", "admin")
