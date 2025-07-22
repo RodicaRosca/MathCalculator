cca684c5118cf34e30a0520b3662f33005c13a5f
Rodica Elena Rosca <Rodica-Elena.Rosca@endava.com>
2025-07-22 11:15:22 +0300

    Added signup

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Form, Depends, HTTPException
+from fastapi import APIRouter, Form, Depends, HTTPException, status
 from jose import jwt
 import datetime
 from sqlalchemy.orm import Session
 from auth import authenticate_user, get_db, SECRET_KEY, ALGORITHM
+from models.request_log import User
+from db.database import SessionLocal
+from passlib.context import CryptContext
 
 router = APIRouter()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/token")
 def login(
@@ -22,3 +26,24 @@ def login(
     }
     token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
     return {"access_token": token, "token_type": "bearer"}
+
+@router.post("/signup")
+def signup(
+    username: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.username == username).first()
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already registered"
+        )
+
+    hashed_password = pwd_context.hash(password)
+    new_user = User(username=username, hashed_password=hashed_password, role="user")
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return {"msg": f"User '{username}' created successfully!"}
